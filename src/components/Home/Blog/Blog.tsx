f0bfedbdeb81f2d253337b1e4bf03ce8bdc43cc7
sajjad <sajjadsajjad098765@gmail.com>
2025-02-16 00:33:08 +0600

    animation added

--- src/components/Home/Blog/Blog.tsx
+++ src/components/Home/Blog/Blog.tsx
@@ -4,25 +4,37 @@ import BlogCard from "@/components/shared/BlogCard";
 import { Button } from "@/components/ui/button";
 import { useGetAllBlogsQuery } from "@/redux/features/blogs/blogManagement";
 import Link from "next/link";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Blog = () => {
   const { data } = useGetAllBlogsQuery(undefined);
   const blogData = data?.data || [];
 
+  useEffect(() => {
+    AOS.init();
+  }, []);
 
   return (
     <div>
-      <h2 className="text-3xl my-14 font-bold text-center mb-12 dark:text-white text-gray-800 font-space">
+      <h2
+        data-aos="fade-up delay-300 duration-300"
+        className="text-3xl my-14 font-bold text-center mb-12 dark:text-white text-gray-800 font-space"
+      >
         Blogs
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+      <div
+        data-aos="fade-up delay-200 duration-300"
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10"
+      >
         {blogData.length > 0 ? (
           blogData.map((blog: any) => <BlogCard key={blog._id} blog={blog} />)
         ) : (
           <p>No projects available.</p>
         )}
       </div>
-      <div className="flex justify-center mt-10">
+      <div data-aos="fade-up delay-300" className="flex justify-center mt-10">
         <Link href="/blogs">
           <Button className="px-6 py-3 bg-blue-600 text-white font-medium text-lg rounded-lg hover:bg-blue-700 transition duration-300 font-space">
             view All
--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,15 +1,14 @@
-
 const Footer = () => {
-    return (
-      <div>
-        <footer className="bg-gray-100 dark:bg-gray-800 border px-16 rounded-lg dark:text-white text-gray-800 py-5 text-center mt-20 font-space">
-          <div className="flex justify-between ">
-            <p>&copy; Copyright © 2025. All rights reserved..</p>
-            <p>Design and Developed by Sajjad Hossain</p>
-          </div>
-        </footer>
-      </div>
-    );
+  return (
+    <div>
+      <footer className="bg-gray-100 dark:bg-gray-800 border px-16 rounded-lg dark:text-white text-gray-800 py-5 text-center mt-20 font-space">
+        <div className="flex justify-between ">
+          <p>&copy; Copyright © 2025. All rights reserved..</p>
+          <p>Design and Developed by Sajjad Hossain</p>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
